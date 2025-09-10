76860904b23180bc16581e47dd814155dd67ac5a
badyast <mail@danbatec.de>
2025-09-11 00:51:27 +0200

    Update GOHSaveEditor.dpr

--- GOHSaveEditor.dpr
+++ GOHSaveEditor.dpr
@@ -1,17 +1,48 @@
 ﻿program GOHSaveEditor;
 
 uses
-  Vcl.Forms,
   ujachLogAuto, ujachLogMgr,
+  Vcl.Forms, System.SysUtils,
   MainForm in 'MainForm.pas' {FrmMain} ,
   ConquestSave in 'ConquestSave.pas';
 
 {$R *.res}
 
 begin
-  Application.Initialize;
-  Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TFrmMain, FrmMain);
-  Application.Run;
+  // Startup-Logging
+  jachLog.LogInfo('=== GOH Save Editor gestartet ===');
+  jachLog.LogInfo('Version: 1.0.0');
+  //jachLog.LogInfo('Build-Datum: %s', [{$I %DATE%}]);
+  jachLog.LogInfo('Plattform: %s',
+    [{$IFDEF WIN64}'Win64'{$ELSE}'Win32'{$ENDIF}]);
+  jachLog.LogInfo('Build-Modus: %s',
+    [{$IFDEF DEBUG}'Debug'{$ELSE}'Release'{$ENDIF}]);
+
+  // System-Basisinfos
+  jachLog.LogInfo('OS: %s', [TOSVersion.ToString]);
+  jachLog.LogInfo('Anwendungspfad: %s', [Application.ExeName]);
+  jachLog.LogInfo('Arbeitsverzeichnis: %s', [GetCurrentDir]);
+
+  try
+    jachLog.LogDebug('Initialisiere Delphi Application Framework');
+    Application.Initialize;
+    Application.MainFormOnTaskbar := True;
+
+    jachLog.LogDebug('Erstelle Hauptformular');
+    Application.CreateForm(TFrmMain, FrmMain);
+
+    jachLog.LogInfo('Starte Anwendungs-Message-Loop');
+    Application.Run;
+
+  except
+    on E: Exception do
+    begin
+      jachLog.LogCritical('KRITISCHER STARTUP-FEHLER: %s', [E.Message]);
+      jachLog.LogError('Startup Exception Details', E);
+      raise;
+    end;
+  end;
+
+  jachLog.LogInfo('=== GOH Save Editor beendet ===');
 
 end.
